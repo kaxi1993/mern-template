51b7d26010a96efaa36c5d4fc1ace3294ca60c46
Lasha Kakhidze <kakhidze2012@gmail.com>
2021-02-20 23:13:27 +0400

    fix import warnings

--- client/index.tsx
+++ client/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App';
+import App from './App/App';
 import store from './store';
 
 render(
