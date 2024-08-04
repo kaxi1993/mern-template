7b3333d0445d3eec4f30c563f6543ecc71571a01
Lasha Kakhidze <kaxi1993@Lasha.local>
2024-08-04 12:53:29 +0400

    Remove client

--- server/auth/auth-controller.js
+++ server/auth/auth-controller.js
@@ -3,9 +3,6 @@ const passport = require('passport')
 const signale = require('signale')
 
 const User = require('../users/user-model')
-const {
-    sendEmail
-} = require('./auth-service')
 
 const tokenize = (sub) => {
     const iat = Date.now()
@@ -67,94 +64,6 @@ const logIn = async (req, res) => {
         })
     } catch (e) {
         signale.fatal('Error occured in login', e)
-
-        res.status(500).send('Internal server error')
-    }
-}
-
-const forgot = async (req, res) => {
-    const {
-        email
-    } = req.body
-
-    try {
-        const user = await User.findOne({
-            email
-        })
-
-        if (!user) {
-            return res.json({
-                message: 'User with this email address not found',
-                status: 'fail',
-                field: 'email'
-            })
-        }
-
-        const token = tokenize(email)
-
-        await sendEmail(email, token)
-
-        res.json({
-            status: 'ok',
-            message: 'Instruction for resetting your password has been sent to your email address'
-        })
-    } catch (e) {
-        signale.fatal('Error occured in forgot', e)
-
-        res.status(500).send('Internal server error')
-    }
-}
-
-const reset = async (req, res) => {
-    const {
-        password,
-        token
-    } = req.body
-
-    let email
-
-    try {
-        const {
-            sub,
-            exp
-        } = jwt.decode(token, process.env.JWT_SECRET)
-
-        if (exp <= Date.now()) {
-            return res.json({
-                message: 'Password reset token expired',
-                status: 'fail'
-            })
-        }
-
-        email = sub
-    } catch (e) {
-        return res.json({
-            message: 'Password reset token invalid',
-            status: 'fail'
-        })
-    }
-
-    try {
-        const user = await User.findOne({
-            email
-        })
-
-        user.password = password
-
-        await user.save()
-
-        const authToken = tokenize(user._id)
-
-        res.json({
-            status: 'ok',
-            token: authToken,
-            user: {
-                email,
-                name: user.name
-            }
-        })
-    } catch (e) {
-        signale.fatal('Error occured in reset', e)
 
         res.status(500).send('Internal server error')
     }
